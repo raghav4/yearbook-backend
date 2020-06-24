65a1ce9b679f27c228e71d9eaec2e458036be059
Raghav Sharma <raghavsyt@gmail.com>
2020-06-24 22:43:21 +0530

    fix: fix profile picture route
    Signed-off-by: Raghav Sharma <raghavsyt@gmail.com>

--- startup/logging.js
+++ startup/logging.js
@@ -1,12 +1,20 @@
 require('express-async-errors');
+const path = require('path');
 const morgan = require('morgan');
 const winston = require('winston');
+const rfs = require('rotating-file-stream');
 winston.transports.DailyRotateFile = require('winston-daily-rotate-file');
 
 module.exports = (app) => {
   if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
   }
+  const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d',
+    path: path.join(__dirname, '../logs/api_hits'),
+  });
+  app.use(morgan('combined', { stream: accessLogStream }));
+
   winston.exceptions.handle(
     new winston.transports.Console({ colorize: true, prettyPrint: true }),
     new winston.transports.DailyRotateFile({
