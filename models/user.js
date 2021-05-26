74e0453f9190a17b12db35151838d662accfef12
Restyled.io <commits@restyled.io>
2021-05-26 12:44:40 +0000

    Restyled by prettier

--- models/user.js
+++ models/user.js
@@ -3,61 +3,63 @@ const mongoose = require('mongoose');
 require('mongoose-type-url');
 const jwt = require('jsonwebtoken');
 
-const Schema = new mongoose.Schema({
-  userId : {
-    type : String,
-    required : true,
-  },
-  password : {
-    type : String,
-    required : true,
-  },
-  name : {
-    type : String,
-    required : true,
-  },
-  bio : {
-    type : String,
-    default : '',
-  },
-  profilePicture : {
-    type : mongoose.SchemaTypes.Url,
-    default : 'https://i.imgur.com/rhLiM4Dm.png',
-  },
-  department : {
-    type : String,
-    enum : {
-      values : [ 'CSE', 'IT', 'MAE', 'ECE', 'EEE' ],
-      message : 'The allowed departments are CSE, IT, MAE, ECE, EEE.',
+const Schema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
     },
-    uppercase : true,
-    required : true,
-  },
-  section : {
-    type : String,
-    enum : {
-      values : [ 'A', 'B', 'C' ],
-      message : 'Allowed Sections are A, B & C',
+    password: {
+      type: String,
+      required: true,
     },
-    uppercase : true,
-    required : true,
+    name: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: '',
+    },
+    profilePicture: {
+      type: mongoose.SchemaTypes.Url,
+      default: 'https://i.imgur.com/rhLiM4Dm.png',
+    },
+    department: {
+      type: String,
+      enum: {
+        values: ['CSE', 'IT', 'MAE', 'ECE', 'EEE'],
+        message: 'The allowed departments are CSE, IT, MAE, ECE, EEE.',
+      },
+      uppercase: true,
+      required: true,
+    },
+    section: {
+      type: String,
+      enum: {
+        values: ['A', 'B', 'C'],
+        message: 'Allowed Sections are A, B & C',
+      },
+      uppercase: true,
+      required: true,
+    },
+    socialHandles: {
+      email: { type: String, default: '' },
+      phone: { type: String, default: '' },
+      linkedin: { type: String, default: '' },
+      instagram: { type: String, default: '' },
+      facebook: { type: String, default: '' },
+      snapchat: { type: String, default: '' },
+    },
   },
-  socialHandles : {
-    email : {type : String, default : ''},
-    phone : {type : String, default : ''},
-    linkedin : {type : String, default : ''},
-    instagram : {type : String, default : ''},
-    facebook : {type : String, default : ''},
-    snapchat : {type : String, default : ''},
+  {
+    collection: 'User',
   },
-},
-                                   {
-                                     collection : 'User',
-                                   });
+);
 
 // eslint-disable-next-line func-names
-Schema.methods.generateAuthToken = function() {
-  return jwt.sign({_id : this._id}, process.env.jwtPrivateKey);
+Schema.methods.generateAuthToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.jwtPrivateKey);
 };
 
 module.exports = mongoose.model('User', Schema);
