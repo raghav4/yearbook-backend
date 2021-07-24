cd48ed82f96fb3e8ebaa2687b93fa149dc257b55
Restyled.io <commits@restyled.io>
2021-07-24 05:25:16 +0000

    Restyled by clang-format

--- models/message.js
+++ models/message.js
@@ -1,31 +1,31 @@
 const mongoose = require('mongoose');
 
 const Message = mongoose.model(
-  'Message',
-  new mongoose.Schema({
-    receiverId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    senderId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-    },
-    isAnonymous: {
-      type: Boolean,
-      default: false,
-    },
-  }),
+    'Message',
+    new mongoose.Schema({
+      receiverId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true,
+      },
+      senderId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true,
+      },
+      content : {
+        type : String,
+        required : true,
+      },
+      isDeleted : {
+        type : Boolean,
+        default : false,
+      },
+      isAnonymous : {
+        type : Boolean,
+        default : false,
+      },
+    }),
 );
 
 module.exports = Message;
--- validation/index.js
+++ validation/index.js
@@ -4,43 +4,43 @@ Joi.objectId = require('joi-objectid')(Joi);
 class Validation {
   static login(user) {
     const schema = Joi.object({
-      userId: Joi.string().length(11).label('User ID').required(),
-      password: Joi.string().label('Password').required(),
+      userId : Joi.string().length(11).label('User ID').required(),
+      password : Joi.string().label('Password').required(),
     });
     return schema.validate(user);
   }
 
   static signup(user) {
     const schema = Joi.object({
-      name: Joi.string().label('Name').required(),
-      password: Joi.string().label('Password').required(),
-      userId: Joi.string().length(11).label('User Id').required(),
-      section: Joi.string().label('Section').required(),
-      department: Joi.string().label('Department').required(),
+      name : Joi.string().label('Name').required(),
+      password : Joi.string().label('Password').required(),
+      userId : Joi.string().length(11).label('User Id').required(),
+      section : Joi.string().label('Section').required(),
+      department : Joi.string().label('Department').required(),
     });
     return schema.validate(user);
   }
 
   static message(messageContent) {
     const schema = Joi.object({
-      content: Joi.string().label('Message Content').required(),
-      receiverId: Joi.objectId().label('Receiver Id').required(),
-      isAnonymous: Joi.boolean().label('Is Anonymous'),
+      content : Joi.string().label('Message Content').required(),
+      receiverId : Joi.objectId().label('Receiver Id').required(),
+      isAnonymous : Joi.boolean().label('Is Anonymous'),
     });
     return schema.validate(messageContent);
   }
 
   static answer(answerContent) {
     const schema = Joi.object({
-      titleId: Joi.objectId().label('titleId').required(),
-      content: Joi.string().label('Answer Content').required(),
+      titleId : Joi.objectId().label('titleId').required(),
+      content : Joi.string().label('Answer Content').required(),
     });
     return schema.validate(answerContent);
   }
 
   static title(title) {
     const schema = Joi.object({
-      title: Joi.string().label('Title').required(),
+      title : Joi.string().label('Title').required(),
     });
     return schema.validate(title);
   }
