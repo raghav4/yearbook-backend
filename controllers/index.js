42a829da13ec3f1f31da16579c7d528b95b69977
Restyled.io <commits@restyled.io>
2021-07-24 05:25:18 +0000

    Restyled by prettier

--- controllers/index.js
+++ controllers/index.js
@@ -5,15 +5,15 @@ const config = require('config');
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 const imgBBUploader = require('imgbb-uploader');
-const {ObjectId} = require('mongoose').Types;
+const { ObjectId } = require('mongoose').Types;
 // eslint-disable-next-line object-curly-newline
-const {User, Answer, Message, Question, Poll, Admin} = require('../models');
+const { User, Answer, Message, Question, Poll, Admin } = require('../models');
 
 class Controller {
   static async userLogIn(req, res) {
-    const {userId, password} = req.body;
+    const { userId, password } = req.body;
 
-    const user = await User.findOne({userId});
+    const user = await User.findOne({ userId });
     if (!user) {
       return res.status(400).send('Invalid userId or Password');
     }
@@ -29,8 +29,8 @@ class Controller {
 
   static async userSignUp(req, res) {
     // eslint-disable-next-line object-curly-newline
-    const {userId, name, password, department, section} = req.body;
-    const data = await User.findOne({userId});
+    const { userId, name, password, department, section } = req.body;
+    const data = await User.findOne({ userId });
     if (data) {
       return res.status(400).send('User Already Exists!');
     }
@@ -46,8 +46,9 @@ class Controller {
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
 
-    return res.status(201).send(
-        _.pick(user, [ 'userId', 'name', 'department', 'section' ]));
+    return res
+      .status(201)
+      .send(_.pick(user, ['userId', 'name', 'department', 'section']));
   }
 
   static async getLoggedInUser(req, res) {
@@ -56,27 +57,32 @@ class Controller {
       return res.status(404).send('User not found!');
     }
     delete user.password;
-    return res.status(200).send(
-        _.pick(user,
-               [
-                 'name', 'userId', 'profilePicture', '_id', 'bio',
-                 'socialHandles', 'department', 'section'
-               ]),
-    );
+    return res
+      .status(200)
+      .send(
+        _.pick(user, [
+          'name',
+          'userId',
+          'profilePicture',
+          '_id',
+          'bio',
+          'socialHandles',
+          'department',
+          'section',
+        ]),
+      );
   }
 
   static async getUserByUserId(req, res) {
-    const user = await User.findOne({userId : req.params.id});
+    const user = await User.findOne({ userId: req.params.id });
     if (!user) {
       return res.status(404).send('Invalid User Id');
     }
-    return res.status(200).send(
-        _.pick(
-            user,
-            [
-              'name', 'userId', 'profilePicture', '_id', 'bio', 'socialHandles'
-            ]),
-    );
+    return res
+      .status(200)
+      .send(
+        _.pick(user, ['name', 'userId', 'profilePicture', '_id', 'bio', 'socialHandles']),
+      );
   }
 
   /**
@@ -90,64 +96,75 @@ class Controller {
 
     const dataToBeUpdated = {};
 
-    const {bio, socialHandles} = req.body;
+    const { bio, socialHandles } = req.body;
 
-    if (bio)
-      dataToBeUpdated.bio = bio;
-    if (socialHandles)
-      dataToBeUpdated.socialHandles = socialHandles;
+    if (bio) dataToBeUpdated.bio = bio;
+    if (socialHandles) dataToBeUpdated.socialHandles = socialHandles;
 
     const socialHandlesObject = {
-      email : socialHandles && socialHandles.email
-                  ? socialHandles.email
-                  : (user.socialHandles && user.socialHandles.email
-                         ? user.socialHandles.email
-                         : null),
-      phone : socialHandles && socialHandles.phone
-                  ? socialHandles.phone
-                  : (user.socialHandles && user.socialHandles.phone
-                         ? user.socialHandles.phone
-                         : null),
-      linkedin : socialHandles && socialHandles.linkedin
-                     ? socialHandles.linkedin
-                     : (user.socialHandles && user.socialHandles.linkedin
-                            ? user.socialHandles.linkedin
-                            : null),
-      instagram : socialHandles && socialHandles.instagram
-                      ? socialHandles.instagram
-                      : (user.socialHandles && user.socialHandles.instagram
-                             ? user.socialHandles.instagram
-                             : null),
-      facebook : socialHandles && socialHandles.facebook
-                     ? socialHandles.facebook
-                     : (user.socialHandles && user.socialHandles.facebook
-                            ? user.socialHandles.facebook
-                            : null),
-      snapchat : socialHandles && socialHandles.snapchat
-                     ? socialHandles.snapchat
-                     : (user.socialHandles && user.socialHandles.snapchat
-                            ? user.socialHandles.snapchat
-                            : null),
+      email:
+        socialHandles && socialHandles.email
+          ? socialHandles.email
+          : user.socialHandles && user.socialHandles.email
+          ? user.socialHandles.email
+          : null,
+      phone:
+        socialHandles && socialHandles.phone
+          ? socialHandles.phone
+          : user.socialHandles && user.socialHandles.phone
+          ? user.socialHandles.phone
+          : null,
+      linkedin:
+        socialHandles && socialHandles.linkedin
+          ? socialHandles.linkedin
+          : user.socialHandles && user.socialHandles.linkedin
+          ? user.socialHandles.linkedin
+          : null,
+      instagram:
+        socialHandles && socialHandles.instagram
+          ? socialHandles.instagram
+          : user.socialHandles && user.socialHandles.instagram
+          ? user.socialHandles.instagram
+          : null,
+      facebook:
+        socialHandles && socialHandles.facebook
+          ? socialHandles.facebook
+          : user.socialHandles && user.socialHandles.facebook
+          ? user.socialHandles.facebook
+          : null,
+      snapchat:
+        socialHandles && socialHandles.snapchat
+          ? socialHandles.snapchat
+          : user.socialHandles && user.socialHandles.snapchat
+          ? user.socialHandles.snapchat
+          : null,
     };
 
     dataToBeUpdated.socialHandles = socialHandlesObject;
 
     user = await User.findByIdAndUpdate(
-        req.user._id,
-        {
-          ...dataToBeUpdated,
-        },
-        {
-          new : true,
-        },
+      req.user._id,
+      {
+        ...dataToBeUpdated,
+      },
+      {
+        new: true,
+      },
     );
-    return res.status(200).send(
-        _.pick(user,
-               [
-                 '_id', 'name', 'userId', 'profilePicture', 'bio',
-                 'socialHandles', 'department', 'section'
-               ]),
-    );
+    return res
+      .status(200)
+      .send(
+        _.pick(user, [
+          '_id',
+          'name',
+          'userId',
+          'profilePicture',
+          'bio',
+          'socialHandles',
+          'department',
+          'section',
+        ]),
+      );
   }
 
   // eslint-disable-next-line consistent-return
@@ -159,15 +176,15 @@ class Controller {
     if (req.files === null) {
       return res.status(400).send('Please attach a valid file');
     }
-    const {file} = req.files;
+    const { file } = req.files;
     file.mv(`${__dirname}/../tmp/${file.name}`, async (err) => {
       if (err) {
         return res.status(500).send(err);
       }
       try {
-        const {url} = await imgBBUploader(
-            config.get('IMGBBKEY'),
-            `${__dirname}/../tmp/${file.name}`,
+        const { url } = await imgBBUploader(
+          config.get('IMGBBKEY'),
+          `${__dirname}/../tmp/${file.name}`,
         );
         user.profilePicture = url;
         await user.save();
@@ -185,24 +202,27 @@ class Controller {
   static async getAllUsersOfAClass(req, res) {
     const user = await User.findById(req.user._id);
     const users = await User.find({
-      $and : [
-        {_id : {$ne : req.user._id}},
-        {department : user.department, section : user.section},
+      $and: [
+        { _id: { $ne: req.user._id } },
+        { department: user.department, section: user.section },
       ],
     });
-    return res.status(200).send(
-        users.map((datum) => _.pick(datum,
-                                    [
-                                      'name',
-                                      'userId',
-                                      'profilePicture',
-                                      '_id',
-                                      'bio',
-                                      'socialHandles',
-                                      'department',
-                                      'section',
-                                    ])),
-    );
+    return res
+      .status(200)
+      .send(
+        users.map((datum) =>
+          _.pick(datum, [
+            'name',
+            'userId',
+            'profilePicture',
+            '_id',
+            'bio',
+            'socialHandles',
+            'department',
+            'section',
+          ]),
+        ),
+      );
   }
 
   /**
@@ -210,20 +230,23 @@ class Controller {
    * This function doesn't return 404 in case of no users exist.
    */
   static async getAllUsers(req, res) {
-    const users = await User.find({_id : {$ne : req.user._id}});
-    return res.status(200).send(
-        users.map((datum) => _.pick(datum,
-                                    [
-                                      'name',
-                                      'userId',
-                                      'profilePicture',
-                                      '_id',
-                                      'bio',
-                                      'socialHandles',
-                                      'department',
-                                      'section',
-                                    ])),
-    );
+    const users = await User.find({ _id: { $ne: req.user._id } });
+    return res
+      .status(200)
+      .send(
+        users.map((datum) =>
+          _.pick(datum, [
+            'name',
+            'userId',
+            'profilePicture',
+            '_id',
+            'bio',
+            'socialHandles',
+            'department',
+            'section',
+          ]),
+        ),
+      );
   }
 
   /**
@@ -258,23 +281,25 @@ class Controller {
   // TODO: SAVE UPDATES
 
   static async updateMessage(req, res) {
-    const {content, receiverId, isAnonymous = false} = req.body;
+    const { content, receiverId, isAnonymous = false } = req.body;
     const filter = {
       receiverId,
-      senderId : req.user._id,
+      senderId: req.user._id,
     };
 
-    const anonymousMessages =
-        await Message.find({senderId : req.user._id, isAnonymous : true});
+    const anonymousMessages = await Message.find({
+      senderId: req.user._id,
+      isAnonymous: true,
+    });
 
     if (isAnonymous && anonymousMessages && anonymousMessages.length >= 3) {
       return res.status(400).send('You can only write 3 anonymous messages');
     }
 
     const message = await Message.findOneAndUpdate(
-        filter,
-        {content, isAnonymous},
-        {new : true, upsert : true, setDefaultsOnInsert : true},
+      filter,
+      { content, isAnonymous },
+      { new: true, upsert: true, setDefaultsOnInsert: true },
     );
 
     return res.status(200).send(message);
@@ -285,17 +310,17 @@ class Controller {
    */
   static async deleteMessage(req, res) {
     const filter = {
-      receiverId : req.params.id,
-      senderId : req.user._id,
-      isDeleted : false,
+      receiverId: req.params.id,
+      senderId: req.user._id,
+      isDeleted: false,
     };
 
     const message = await Message.findOneAndUpdate(
-        filter,
-        {
-          isDeleted : true,
-        },
-        {new : true},
+      filter,
+      {
+        isDeleted: true,
+      },
+      { new: true },
     );
 
     if (!message) {
@@ -305,13 +330,16 @@ class Controller {
   }
 
   static async deleteMessageById(req, res) {
-    const message = await Message.findOneAndUpdate({
-      _id : req.params.id,
-      isDeleted : false,
-    },
-                                                   {isDeleted : true}, {
-                                                     new : true,
-                                                   });
+    const message = await Message.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        isDeleted: false,
+      },
+      { isDeleted: true },
+      {
+        new: true,
+      },
+    );
 
     if (!message) {
       return res.status(404).send('Message does not exist!');
@@ -323,30 +351,27 @@ class Controller {
    * Function to get all messages of a user by receiverId.
    */
   static async getAllReceivedMessages(req, res) {
-    const messages = await Message
-                         .find({
-                           receiverId : new ObjectId(req.user._id),
-                           isDeleted : false,
-                         })
-                         .populate('senderId');
+    const messages = await Message.find({
+      receiverId: new ObjectId(req.user._id),
+      isDeleted: false,
+    }).populate('senderId');
 
     if (!messages || (messages && !messages.length)) {
       // Status 200 instead of 404 here.
       return res.status(200).send([]);
     }
 
-    const result =
-        messages
-            .map((message) => ({
-                   ..._.pick(message, [ '_id' ]),
-                   ..._.pick(message, [ 'content' ]),
-                   ..._.pick(message, [ 'isAnonymous' ]),
-                   sender : _.get(message, 'senderId.name'),
-                 }))
-            .map((message) => ({
-                   ...message,
-                   sender : message.isAnonymous ? 'Anonymous' : message.sender
-                 }));
+    const result = messages
+      .map((message) => ({
+        ..._.pick(message, ['_id']),
+        ..._.pick(message, ['content']),
+        ..._.pick(message, ['isAnonymous']),
+        sender: _.get(message, 'senderId.name'),
+      }))
+      .map((message) => ({
+        ...message,
+        sender: message.isAnonymous ? 'Anonymous' : message.sender,
+      }));
     return res.status(200).send(result);
   }
 
@@ -355,16 +380,15 @@ class Controller {
    */
   static async getMessageByReceiverId(req, res) {
     const message = await Message.findOne({
-      receiverId : req.params.id,
-      senderId : req.user._id,
-      isDeleted : false,
+      receiverId: req.params.id,
+      senderId: req.user._id,
+      isDeleted: false,
     });
 
     if (!message) {
       return res.status(404).send('No message found');
     }
-    if (message.isAnonymous)
-      delete message.sender;
+    if (message.isAnonymous) delete message.sender;
     return res.status(200).send(message);
   }
 
@@ -373,29 +397,30 @@ class Controller {
    * !! No 404 for no answers.
    */
   static async getAllAnswersOfAUser(req, res) {
-    const answers = await Answer
-                        .find({
-                          userId : req.user._id,
-                        })
-                        .select('-userId -__v')
-                        .populate('titleId');
-    return res.status(200).send(answers.map((answer) => ({
-                                              _id : answer._id,
-                                              content : answer.content,
-                                              title : answer.titleId.title,
-                                              titleId : answer.titleId._id
-                                            })));
+    const answers = await Answer.find({
+      userId: req.user._id,
+    })
+      .select('-userId -__v')
+      .populate('titleId');
+    return res.status(200).send(
+      answers.map((answer) => ({
+        _id: answer._id,
+        content: answer.content,
+        title: answer.titleId.title,
+        titleId: answer.titleId._id,
+      })),
+    );
   }
 
   /**
    * Function to add a new answer or update existing one.
    */
   static async upsertAnswer(req, res) {
-    const {titleId, content} = req.body;
+    const { titleId, content } = req.body;
     const answer = await Answer.findOneAndUpdate(
-        {titleId, userId : req.user._id},
-        {content},
-        {new : true, upsert : true},
+      { titleId, userId: req.user._id },
+      { content },
+      { new: true, upsert: true },
     );
     return res.status(200).send(answer);
   }
@@ -428,11 +453,13 @@ class Controller {
   static async getAllSlambookQuestions(req, res) {
     const questions = await Question.find({});
     if (!questions) {
-      return res.status(404).send(
-          'Looks like there are no questions for the user to answer!');
+      return res
+        .status(404)
+        .send('Looks like there are no questions for the user to answer!');
     }
-    return res.status(200).send(
-        _.map(questions, _.partialRight(_.pick, [ '_id', 'title' ])));
+    return res
+      .status(200)
+      .send(_.map(questions, _.partialRight(_.pick, ['_id', 'title'])));
   }
 
   /**
@@ -440,14 +467,14 @@ class Controller {
    */
   static async createSlambookQuestion(req, res) {
     let question = await Question.findOne({
-      title : new RegExp(`^${req.body.title}$`, 'i'),
+      title: new RegExp(`^${req.body.title}$`, 'i'),
     });
 
     if (question) {
       return res.status(400).send('Question already exist!');
     }
     question = new Question({
-      title : req.body.title.trim(),
+      title: req.body.title.trim(),
     });
     await question.save();
     return res.status(200).send(question);
@@ -491,7 +518,7 @@ class Controller {
    */
   static async createPoll(req, res) {
     const poll = new Poll({
-      title : req.body.title,
+      title: req.body.title,
     });
 
     await poll.save();
@@ -514,9 +541,9 @@ class Controller {
    * Function to login an admin
    */
   static async adminLogIn(req, res) {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
-    const admin = await Admin.findOne({username});
+    const admin = await Admin.findOne({ username });
     if (!admin) {
       return res.status(400).send('Invalid username or Password');
     }
